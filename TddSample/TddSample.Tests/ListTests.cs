6ee83c31c938f2607a0d8800d670f34ca0aad630
Robert Pająk <pellared@hotmail.com>
2014-07-02 20:27:18 +0200

    TddSample solution

---
+++ TddSample/TddSample.Tests/ListTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace TddSample.Tests
+{
+    public class ListTests
+    {
+        [Fact]
+        public void Add_OneElement_CountReturnsOne()
+        {
+            // Arrange
+            var list = new List<int>();
+
+            // Act
+            list.Add(3);
+
+            // Assert
+            list.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void AddTests()
+        {
+            // COMMON ANTI-PATTERN: The One
+            var list = new List<int>();
+
+            list.Add(3);
+            list.Count.Should().Be(2);
+
+            const int addedElement = 2;
+            list.Add(addedElement);
+            list.Last().Should().Be(addedElement);
+
+            // there should be on unit test for each concept; not for each method!
+
+            // tests should be trustworthy, maintanable and readable!
+            // if there are not then something is wrong with the tests or the code under test
+
+            // anit-patterns: http://blog.james-carr.org/2006/11/03/tdd-anti-patterns/
+        }
+    }
+}
